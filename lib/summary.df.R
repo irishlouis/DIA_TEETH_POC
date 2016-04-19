0a8bc578f7ceb00f730ff9e4d1f8eab725889f5e
irishlouis <louissmith@gmail.com>
2016-04-19 14:13:28 +0100

    tidy

--- lib/summary.df.R
+++ lib/summary.df.R
@@ -7,20 +7,21 @@
 #' @export summary data.frame
 #' 
 summary.df <- function(df, freq, k=10){
-  test <- df
-  times <- unique(test$time_minute)
+  test <-  select(df, time_minute, vector.mag) %>%
+    melt(id.vars = "time_minute")
   
-  test.summary <- select(test, time_minute, vector.mag) %>%
-    melt(id.vars = "time_minute") %>%
+  times <- unique(df$time_minute)
+  
+  test.summary <- test %>%
     group_by(time_minute, variable) %>%
     summarise(min = min(value), 
               Qu1 = quantile(value, .25),
               Median = median(value),
               Mean = mean(value),
               Qu2 = quantile(value, .75),
-              Max = max(value)) 
+              Max = max(value))
   
-  tmp <- do.call(rbind, lapply(times, function(t) peak.func(t, test, freq, k)))
+  tmp <- do.call(rbind, lapply(times, function(t) peak.func(t, df, freq, k)))
   test.summary <- cbind(test.summary, tmp %>% select(-Timestamp)) 
   return(test.summary)
 }
