264139868f4fe30e71b7930761ed3a1e0f7c6ab8
irishlouis <louissmith@gmail.com>
2016-04-19 14:11:49 +0100

    tidy variable names

--- lib/get.sim.results.R
+++ lib/get.sim.results.R
@@ -1,30 +1,34 @@
 #' get.sim.results
 #'
-#' @param summary.df 
+#' @param summ.df 
 #' @param brushing.fingerprint 
 #' @param similarity.euclidean.m
 #' @param close
 #'
 #' @return sim.results
 #' 
-get.sim.results <- function(summary.df, 
+get.sim.results <- function(summ.df, 
                             brushing.fingerprint,
-                            similarity.euclidean.m = similarity.euclidean, 
+                            similarity.measure = similarity.euclidean, 
                             close = 2){
   cores <- detectCores()
   cl <- makeCluster(cores)
   registerDoParallel(cl)
-  times <- unique(summary.df$time_minute)
+  times <- unique(summ.df$time_minute)
   sim.results.e <- foreach(t = seq_along(times), .combine = c) %dopar% {
-    return(sim = similarity.euclidean.m(as.data.frame(brushing.fingerprint)[1,], 
-                                        summary.df[summary.df$time_minute == times[t], ][1,]))
+    return(sim = similarity.measure(brushing.fingerprint = as.data.frame(brushing.fingerprint)[1,], 
+                                    d = summ.df[summ.df$time_minute == times[t], ][1,],
+                                    close = close))
   }
   
   stopCluster(cl)
   
-  sim.results <- data.frame(times = times, sim.e = sim.results.e)
-  
-  sim.results$event.e <- ifelse(sim.results$sim.e < close, 1, 0)
+  sim.results <- data.frame(times = times, sim.result = sim.results.e)
+  if(identical(similarity.measure , similarity.euclidean)) {
+    sim.results$event <- ifelse(sim.results$sim.result < close, 1, 0)
+  } else {
+    sim.results$event <- ifelse(sim.results$sim.result >= 5, 1, 0)
+  }
   
   return(sim.results)
 }
