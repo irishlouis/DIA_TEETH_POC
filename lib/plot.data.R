baa6ac0c86ca30be7835172d2415263d45089c6f
irishlouis <louissmith@gmail.com>
2016-04-19 14:12:18 +0100

    add theme_bw

--- lib/plot.data.R
+++ lib/plot.data.R
@@ -7,11 +7,11 @@
 #'
 #' @examples
 plot.data <- function(d){
-  p1 <- ggplot(d, aes(Timestamp, vector.mag)) + geom_point(aes(col = vector.dir)) + 
-    geom_line() + theme(legend.position="none") + labs(title = "Overall Vector Mag")
-  p2 <- ggplot(d, aes(Timestamp, AccelerometerX)) + geom_line() + labs(title = "X Accel Data")
-  p3 <- ggplot(d, aes(Timestamp, AccelerometerY)) + geom_line() + labs(title = "Y Accel Data")
-  p4 <- ggplot(d, aes(Timestamp, AccelerometerZ)) + geom_line() + labs(title = "Z Accel Data")
+  p1 <- ggplot(d, aes(Timestamp, vector.mag)) + 
+    geom_line() + theme(legend.position="none") + labs(title = "Overall Vector Mag") + theme_bw()
+  p2 <- ggplot(d, aes(Timestamp, AccelerometerX)) + geom_line() + labs(title = "X Accel Data") + theme_bw()
+  p3 <- ggplot(d, aes(Timestamp, AccelerometerY)) + geom_line() + labs(title = "Y Accel Data") + theme_bw()
+  p4 <- ggplot(d, aes(Timestamp, AccelerometerZ)) + geom_line() + labs(title = "Z Accel Data") + theme_bw()
   
   grid.arrange(p1, p2, p3, p4, ncol = 1)
 }
