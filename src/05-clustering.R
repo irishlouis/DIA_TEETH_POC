9a4e4864bffdfd78b02aa291820b55c289b3ad85
irishlouis <louissmith@gmail.com>
2016-04-19 14:15:05 +0100

    tidy

--- src/05-clustering.R
+++ src/05-clustering.R
@@ -1,39 +1,45 @@
 # trying dbscan clustering
 
 ## make wide datasets
-test.summary.wide <- melt(test.summary %>% rename(metric = variable), id.vars = c("Timestamp", "metric")) %>% 
-  dcast(Timestamp ~ metric + variable, fill = 0) 
+df.summary.wide <- melt(df.summary %>% rename(metric = variable), id.vars = c("time_minute", "metric")) %>% 
+  dcast(time_minute ~ metric + variable, fill = 0) 
 
-times <- filter(test.results, event >0) %>% select(time_minute) %>% distinct
-test.summary.wide <- test.summary.wide %>% filter(Timestamp %in% times$time_minute)
+times <- filter(df.summary, brushing >0) %>% select(time_minute) %>% distinct
 
 ## train pca model
-pca.mdl <- preProcess(test.summary.wide %>% select(-Timestamp), method = "pca", pcaComp = 2)
+pca.mdl <- preProcess(df.summary.wide %>% select(-time_minute, -vector.mag_brushing), method = "pca", pcaComp = 2)
 
-## apply pca model, keep timestamp
-test.summary.wide.pca <- cbind(Timestamp = test.summary.wide$Timestamp, 
-                               predict(pca.mdl, test.summary.wide %>% select(-Timestamp)))
+## apply pca model, keep timestamp and brushing flag
+df.summary.wide.pca <- cbind(time_minute = df.summary.wide$time_minute,
+                               brushing = df.summary.wide$vector.mag_brushing,
+                               predict(pca.mdl, df.summary.wide %>% select(-time_minute, -vector.mag_brushing)))
 
 ## plot results
-ggplot(test.summary.wide.pca %>% mutate(flag = ifelse(Timestamp %in% times$time_minute, 1, 0)),
-       aes(PC1, PC2)) + geom_point(aes(col = as.factor(flag)))
+ggplot(df.summary.wide.pca,
+       aes(PC1, PC2)) + geom_point(aes(col = as.factor(brushing)))
 
-## apply dbscan to pca data
-d.cluster <- dbscan(test.summary.wide.pca %>% select(-Timestamp), eps = .05, MinPts = 3, scale = TRUE)
+## apply dbscan to data
+set.seed(234)
+d.cluster <- dbscan(df.summary.wide %>% select(-time_minute, -vector.mag_brushing), 
+                    eps = .1, MinPts = 3, scale = TRUE)
 ## plot results
-ggplot(cbind(cluster = d.cluster$cluster, test.summary.wide.pca), aes(PC1, PC2)) + geom_point(aes(col = as.factor(cluster)))
+ggplot(cbind(cluster = d.cluster$cluster, df.summary.wide.pca), aes(PC1, PC2)) + geom_point(aes(col = as.factor(cluster)))
 
-cbind(cluster = d.cluster$cluster, Timestamp = as.character(test.summary.wide$Timestamp)) %>% 
-  data.frame %>% filter(Timestamp %in% as.character(times$time_minute))
+# what cluster are brushing events in
+cbind(cluster = d.cluster$cluster, time_minute = as.character(df.summary.wide$time_minute)) %>% 
+  data.frame %>% filter(time_minute %in% as.character(times$time_minute))
 
-cbind(cluster = d.cluster$cluster, Timestamp = as.character(test.summary.wide$Timestamp)) %>% 
-  data.frame %>% filter(cluster == 1)
 
 # kmeans
-k.cluster <- kmeans(test.summary.wide %>% select(-Timestamp), centers = 25)
+set.seed(234)
+k.cluster <- kmeans(df.summary.wide %>% select(-time_minute, -vector.mag_brushing), centers = 25)
 table(k.cluster$cluster)
-cbind(cluster = k.cluster$cluster, Timestamp = as.character(test.summary.wide$Timestamp)) %>% 
-  data.frame %>% filter(Timestamp %in% as.character(times$time_minute))
 
-cbind(cluster = k.cluster$cluster, Timestamp = as.character(test.summary.wide$Timestamp)) %>% 
-  data.frame %>% filter(cluster == 21)
+# what cluster are events in
+cbind(cluster = k.cluster$cluster, time_minute = as.character(df.summary.wide$time_minute)) %>% 
+  data.frame %>% filter(time_minute %in% as.character(times$time_minute))
+
+# cluster 8 looks promising
+cbind(cluster = k.cluster$cluster, time_minute = as.character(df.summary.wide$time_minute)) %>% 
+  data.frame %>% filter(cluster == 8)
+
