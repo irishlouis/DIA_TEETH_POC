3ce7c02452def0d109d212f724a29c015e9327ca
irishlouis <louissmith@gmail.com>
2016-05-17 13:57:01 +0100

    change function to return validation and new subj, add new subj kappa plots

--- src/08-ml.results.resampled.R
+++ src/08-ml.results.resampled.R
@@ -1,6 +1,8 @@
-# run with multiple splits to see if performance holds
+# run with multiple splits to see if performance holds for new data/different subject
 
-checkModel <- function(seed){
+testing2 <- test.new.summary[,brushing := as.factor(ifelse(brushing == 1, "y", "n")),]
+
+checkModelNewSubj <- function(seed=1){
   set.seed(seed)
   # partition data for building model
   inTrain <- createDataPartition(df.summary$brushing, p = .6, list = FALSE)
@@ -28,10 +30,16 @@ checkModel <- function(seed){
                           methodList=c('xgbTree', 'ada', 'gbm',
                                        'adaboost', 'rf', 'nnet', 
                                        'svmRadial', 'rpart'))
+  rm(training)
   
-  return(lapply(model_list, 
-                function(x) confusionMatrix(predict(x, testing), testing$brushing)$overall[2]) %>% unlist)
+  return(rbind(lapply(model_list, 
+                      function(x) confusionMatrix(predict(x, testing), testing$brushing)$overall[2]) %>% unlist,
+               lapply(model_list, 
+                      function(x) confusionMatrix(predict(x, as.data.frame(testing2)[, -c(1:2)]), testing2$brushing)$overall[2]) %>% unlist))
+  rm(model_list)
   stopCluster(cl)
+  
+  unregister()
 }
 
 set.seed(123123)
@@ -39,13 +47,20 @@ set.seed(123123)
 n_resamples <- 30
 seeds <- round(rep(runif(n_resamples, 1, 10000000)), 0)
 
-resample.results <- do.call(rbind, lapply(seeds, function(s) {print(which(seeds ==s)); return(checkModel(s))}))
+resample.results <- do.call(rbind, lapply(seeds, function(s) {print(which(seeds ==s)); return(checkModelNewSubj(s))}))
 
-summary(resample.results)
+cache("resample.results")
 
-ggplot(melt(resample.results, varnames = c("sample_id", "method"), value.name = "kappa") %>% 
-         mutate(method = str_replace(method, ".Kappa", "")),
-       aes(x=method, y=kappa)) + 
+dt <- data.table(resample.results)
+dt[,test_type := c("validation", "new_subj"),]
+
+dt[,lapply(.SD, summary), test_type]
+
+setkey(dt, test_type)
+
+ggplot(melt(dt["validation"], id.vars = "test_type", value.name = "kappa") %>% 
+         mutate(method = str_replace(variable, ".Kappa", "")),
+       aes(x=variable, y=kappa)) + 
   geom_violin() +
   geom_boxplot(fill = "grey", alpha = 0.25) + 
   geom_point(alpha = 0.25) +
@@ -59,4 +74,19 @@ ggplot(melt(resample.results, varnames = c("sample_id", "method"), value.name = 
   scale_y_continuous(labels = percent)
          
   
+ggplot(melt(dt["new_subj"], id.vars = "test_type", value.name = "kappa") %>% 
+         mutate(method = str_replace(variable, ".Kappa", "")),
+       aes(x=variable, y=kappa)) + 
+  geom_violin() +
+  geom_boxplot(fill = "grey", alpha = 0.25) + 
+  geom_point(alpha = 0.25) +
+  theme_bw() +
+  labs(title = "Kappa Results from Models generalised to new subject for 30 partitions",
+       subtitle = "Significant variation in performance",
+       x="",
+       y="Kappa") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
+        panel.grid.major.x = element_blank()) +
+  scale_y_continuous(labels = percent)
+
 cache("resample.results")
